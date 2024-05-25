456a28c3adeae33af34b117ac9a2939b2eef6707
Grant Gremillion <grg011@latech.edu>
2024-05-24 20:36:11 -0500

    Added basic player movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Rigidbody2D rb;
+    private Vector2 movementInput;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = movementInput * speed;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        movementInput = inputValue.Get<Vector2>();
+
+    }
+}
