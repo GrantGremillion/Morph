93e758836bcb9d59cec8d827acf61bad2da0b4f9
Grant Gremillion <grg011@latech.edu>
2024-05-25 21:05:40 -0500

    Added simple health system

--- Assets/Scripts/UI/InGameUI.cs
+++ Assets/Scripts/UI/InGameUI.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class InGameUI : MonoBehaviour
 {
 
+    // Item UI variables
     public TextMeshProUGUI itemText;
     public InventoryManager inventoryManager;
+
+
+    // Health UI variables
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+
+    public PlayerController player;
+
 
 
     // Start is called before the first frame update
@@ -19,6 +32,33 @@ public class InGameUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        for (int i=0; i < hearts.Length; i++)
+        {
+
+            if(player.health > player.numOfHearts)
+            {
+                player.health = player.numOfHearts;
+            }
+
+            if (i < player.health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < player.numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
         
     }
 
