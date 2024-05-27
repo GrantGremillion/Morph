9139a6bb0c19cb1a2cf0d7fcc580974627b764fc
Grant Gremillion <grg011@latech.edu>
2024-05-27 00:50:22 -0500

    Player can't damage an idle enemy

--- Assets/Scripts/Enemies/ProjectileTrigger.cs
+++ Assets/Scripts/Enemies/ProjectileTrigger.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Data;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class ProjectileTrigger : MonoBehaviour
 {
     private Enemy enemy;
+    private Collider2D enemyCollider;
+    public float disableDuration = 2.0f; // Duration to disable the collider
 
     // Start is called before the first frame update
     void Start()
     {
         enemy = GetComponentInParent<Enemy>();
+        if (enemy != null)
+        {
+            enemyCollider = enemy.GetComponent<Collider2D>();
+            if (enemyCollider == null)
+            {
+                Debug.LogError("No Collider2D component found on the Enemy.");
+            }
+        }
+        else
+        {
+            Debug.LogError("No Enemy component found in parent.");
+        }
     }
 
     // Update is called once per frame
@@ -19,15 +34,24 @@ public class ProjectileTrigger : MonoBehaviour
         
     }
 
-    void OnTriggerEnter2D (Collider2D collider)
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        print(enemy.currentState);
-        
+        if (enemy == null || enemyCollider == null)
+        {
+            Debug.LogError("Enemy or its Collider2D component is not set properly.");
+            return;
+        }
+
         if (collider.CompareTag("Arrow") && enemy.currentState == Enemy.State.Idle)
         {
-            Destroy(collider.gameObject);
+            StartCoroutine(DisableColliderTemporarily());
         }
     }
 
-
+    private IEnumerator DisableColliderTemporarily()
+    {
+        enemyCollider.enabled = false;
+        yield return new WaitForSeconds(disableDuration);
+        enemyCollider.enabled = true;
+    }
 }
