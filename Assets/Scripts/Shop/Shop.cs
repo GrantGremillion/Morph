96ac185cf448e3c7d8dfc6f2419676c91cc35664
Grant Gremillion <grg011@latech.edu>
2024-05-27 22:08:07 -0500

    Added basic tilemap

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -13,7 +13,6 @@ public class Shop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        shopUI = FindAnyObjectByType<ShopUI>();
         shopUI.gameObject.SetActive(false);
     }
 
